b71b0bbd16eb283237fd523cd613c9e50417348b
saadee <saadee7223@gmail.com>
2021-07-15 00:58:19 +0500

    adding smooth transition

--- SuperProps - React Next Landing Page Templates/Superprops/Superprops/packages/landing/src/pages/index.js
+++ SuperProps - React Next Landing Page Templates/Superprops/Superprops/packages/landing/src/pages/index.js
@@ -66,7 +66,7 @@ const Main = () => {
             <Banner />
             
             
-            <RoadMap/>
+            {/* <RoadMap/> */}
             <Tokenomics/>
             
             <ParallaxProvider>
