4967dcbac50ffd04f70be6826fd879a937d9f2f3
jcoelho@ua.pt <jcoelho@ua.pt>
2021-07-14 16:50:10 +0100

    tokenomics

---
+++ src/containers/CryptoModern/Tokenomics/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from 'common/components/Box';
+import Text from 'common/components/Text';
+import Heading from 'common/components/Heading';
+import FeatureBlock from 'common/components/FeatureBlock';
+import { Features } from 'common/data/CryptoModern';
+import Container from 'common/components/UI/Container';
+
+const TokenomicsSection = ({
+  row,
+  col,
+  sectionHeader,
+  sectionTitle,
+  sectionSubTitle,
+  featureTitle,
+  featureDescription,
+  iconStyle,
+  contentStyle,
+  blockWrapperStyle,
+}) => {
+  return (
+    <>
+        <h2>News in fashion</h2>
+        
+    </>
+  );
+};
+
+
+export default TokenomicsSection;
+
+
