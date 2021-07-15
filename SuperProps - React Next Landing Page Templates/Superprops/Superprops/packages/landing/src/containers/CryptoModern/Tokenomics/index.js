04fea5b3befa328af75a4fa17b16491b64ddbac8
saadee <saadee7223@gmail.com>
2021-07-15 19:15:26 +0500

    cards done

--- SuperProps - React Next Landing Page Templates/Superprops/Superprops/packages/landing/src/containers/CryptoModern/Tokenomics/index.js
+++ SuperProps - React Next Landing Page Templates/Superprops/Superprops/packages/landing/src/containers/CryptoModern/Tokenomics/index.js
@@ -10,7 +10,11 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Image from 'next/image'
 // import uncleBoss from 'common/assets/image/agency/uncleBoss.png';
-import uncleBoss from 'common/assets/image/zeus-1.jpg';
+import Zeus from 'common/assets/image/zeus-1.jpg';
+import Poseidon from 'common/assets/image/poseidon.jpg';
+import Plutus from 'common/assets/image/plutus.jpg';
+import Favor from 'common/assets/image/favor.jpg';
+import Hades from 'common/assets/image/hades.jpg';
 
 // import './tokenomics.css'
 
@@ -36,13 +40,13 @@ const useStyles = makeStyles((theme) => ({
     padding: '11%',
     minHeight: '500px',
     position: 'relative',
-    '&: hover':{
+    '&: hover': {
       transition: 'all 6s ease-in-out',
-transform: 'scale(1.2)',
+      transform: 'scale(1.2)',
 
     }
   },
- 
+
 }));
 const TokenomicsSection = ({
   row,
@@ -57,92 +61,55 @@ const TokenomicsSection = ({
   blockWrapperStyle,
 }) => {
   const classes = useStyles();
+  const details = [
+    {
+      title: '4% of Zeus',
+      detail: 'The mighty Zeus rests on his throne, ready to strike down and buy back from those who would challenge Olympus’s legacy. 4% of all transactions are used to fund the buyback power of Zeus.',
+      id: 'zeus',
+      image: Zeus
+    },
+    {
+      title: '4% to Poseidon',
+      detail: 'With each trade, 4% is added to the liquidity pool to calm the waters. However, to keep balance and order in Olympus, when liquidity is above 25% of the total market cap this tax will be paid to Zeus as tribute, lifting Olympus back to its proper place in the heavens, the first dynamic liquidity generation of its kind.',
+      id: 'poseidon',
+      image: Poseidon
+    },
+    {
+      title: '4% of Plutus',
+      detail: '4% to Plutus The God of Wealth, Plutus bestows wealth upon the worshipers of Olympus, rewarding their faith in the Omnipotent Zeus. 4% of each transaction will be converted to BUSD and sent automatically to the wallets of the followers of Zeus.',
+      id: 'plutus',
+      image: Plutus
+    },
+    {
+      title: '2% of Favour',
+      detail: '2% to Favour In order to further explore the full potential of Olympus, 2% will be sent to a wallet as favour for development and marketing purposes, controlled by the gods themselves.',
+      id: 'favour',
+      image: Favor
+    },
+    {
+      title: 'Tribute to Hades',
+      detail: 'Tribute to Hades Having been thrown out of Olympus, betrayed by his own brothers and sisters, Hades rules the underworld, feeding on the souls of the lost. After each buyback, transaction fees FOR SELLS are doubled for 30 minutes, descending to the original rate with each minute',
+      id: 'hades',
+      image: Hades
+    },
+  ]
   return (
-    <div className={classes.root}>
-      <Grid container alignContent='center' spacing={3} >
-        <Grid item xs={12} lg={2} md={3}>
-          <div className='wrapper'>
-          <div className='zeusDiv' style={{backgroundImage: "url(" + `${require("common/assets/image/zeus-1.jpg")}` + ")"}} >
-            <div className='contentDiv'>
-              <div className='content'>
-
-              <h2 className='heading'> 4% to Zeus</h2>
-              <p className='extraContent'>
-                The mighty Zeus rests on his throne, ready to strike down and buy back from those who would challenge Olympus’s legacy. 4% of all transactions are used to fund the buyback power of Zeus.
+    <div>
+      <div className=" row text-center p-3">
+        {details?.map((item) => (
+          <div className="col-lg-2 col-md-12 col-sm-12 col-xs-12 container_foto ">
+            <article className="text-left">
+              <h2>{item.title}</h2>
+            </article>
+            <div className="ver_mas text-center" id={item.id}>
+              <p className="lnr lnr-eye">
+                {item.detail}
               </p>
-              </div>
             </div>
+            <img src={item.image} alt="" />
           </div>
-            </div>
-        </Grid>
-        <Grid item xs={12} lg={2} md={3}>
-          <div className='wrapper'>
-          <div className='zeusDiv' style={{backgroundImage: "url(" + `${require("common/assets/image/poseidon.jpg")}` + ")"}} >
-            <div className='contentDiv1'>
-              <div className='content1'>
-
-              <h2 className='heading1'> 4% to Poseidon</h2>
-              <p className='extraContent1'>
-              
-With each trade, 4% is added to the liquidity pool to calm the waters. However, to keep balance and order in Olympus, when liquidity is above 25% of the total market cap this tax will be paid to Zeus as tribute, lifting Olympus back to its proper place in the heavens, the first dynamic liquidity generation of its kind.
-              </p>
-              </div>
-            </div>
-          </div>
-            </div>
-        </Grid>
-        <Grid item xs={12} lg={2} md={3}>
-          <div className='wrapper'>
-          <div className='zeusDiv' style={{backgroundImage: "url(" + `${require("common/assets/image/plutus.jpg")}` + ")"}} >
-            <div className='contentDiv2'>
-              <div className='content2'>
-
-              <h2 className='heading2'> 4% to Plutus</h2>
-              <p className='extraContent2'>
-            
-The God of Wealth, Plutus bestows wealth upon the worshipers of Olympus, rewarding their faith in the Omnipotent Zeus. 4% of each transaction will be converted to BUSD and sent automatically to the wallets of the followers of Zeus.
-              </p>
-              </div>
-            </div>
-          </div>
-            </div>
-        </Grid>
-        <Grid item xs={12} lg={2} md={3}>
-          <div className='wrapper'>
-          <div className='zeusDiv' style={{backgroundImage: "url(" + `${require("common/assets/image/favor.jpg")}` + ")"}} >
-            <div className='contentDiv3'>
-              <div className='content3'>
-
-              <h2 className='heading3'> 4% to Favour</h2>
-              <p className='extraContent3'>
-            
-              
-In order to further explore the full potential of Olympus, 2% will be sent to a wallet as favour for development and marketing purposes, controlled by the gods themselves.
-              </p>
-              </div>
-            </div>
-          </div>
-            </div>
-        </Grid>
-        <Grid item xs={12} lg={2} md={3}>
-          <div className='wrapper'>
-          <div className='zeusDiv' style={{backgroundImage: "url(" + `${require("common/assets/image/hades.jpg")}` + ")"}} >
-            <div className='contentDiv4'>
-              <div className='content4'>
-
-              <h2 className='heading4'> Tribute to Hades</h2>
-              <p className='extraContent4'>
-            
-Having been thrown out of Olympus, betrayed by his own brothers and sisters, Hades rules the underworld, feeding on the souls of the lost. After each buyback, transaction fees FOR SELLS are doubled for 30 minutes, descending to the original rate with each minute
-              </p>
-              </div>
-            </div>
-          </div>
-            </div>
-        </Grid>
-     
-
-      </Grid>
+        ))}
+      </div>
     </div>
   );
 };
